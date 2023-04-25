9de6e70737f340fac76cf535fe87f0b907d3e902
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-04-25 18:05:37 -0300

    feature(server): connecting with mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import "reflect-metadata";
 
 import path from "node:path";
+import mongoose from "mongoose";
+import * as dotenv from 'dotenv';
 
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
@@ -8,6 +10,14 @@ import { AppointmentsResolver } from "./resolvers/appointments-resolver";
 import { CustomerResolver } from "./resolvers/customer-resolver";
 
 async function bootstrap() {
+  dotenv.config();
+  const dbUrl = process.env.DB_URL;
+
+  if (!dbUrl)
+    throw new Error('Please, provide a database url.');
+
+  await mongoose.connect(dbUrl);
+
   const schema = await buildSchema({
     resolvers: [
       AppointmentsResolver,
