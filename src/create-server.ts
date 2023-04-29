b73606acac80f43d11fc201c30d8aa8dc18d3708
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-04-28 21:34:22 -0300

    refactor(server): separating create-server file

---
+++ src/create-server.ts
@@ -0,0 +1,34 @@
+import "reflect-metadata";
+
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from "type-graphql";
+import { AppointmentsResolver } from "./resolvers/appointments-resolver";
+import { CustomerResolver } from "./resolvers/customer-resolver";
+import mongoose from "mongoose";
+import path from "node:path";
+import * as dotenv from 'dotenv';
+
+export async function createServer(): Promise<ApolloServer> {
+  dotenv.config();
+  const dbUrl = process.env.DB_URL;
+
+  if (!dbUrl)
+    throw new Error('Please, provide a database url.');
+
+  await mongoose.connect(dbUrl);
+
+  const schema = await buildSchema({
+    resolvers: [
+      AppointmentsResolver,
+      CustomerResolver,
+    ],
+    emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
+    validate: {
+      forbidUnknownValues: false,
+    }
+  });
+
+  return new ApolloServer({
+    schema,
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,9 @@
-import "reflect-metadata";
-
-import path from "node:path";
-import mongoose from "mongoose";
-import * as dotenv from 'dotenv';
-
-import { ApolloServer } from "apollo-server";
-import { buildSchema } from "type-graphql";
-import { AppointmentsResolver } from "./resolvers/appointments-resolver";
-import { CustomerResolver } from "./resolvers/customer-resolver";
+import { createServer } from './create-server';
 
 async function bootstrap() {
-  dotenv.config();
-  const dbUrl = process.env.DB_URL;
-
-  if (!dbUrl)
-    throw new Error('Please, provide a database url.');
-
-  await mongoose.connect(dbUrl);
-
-  const schema = await buildSchema({
-    resolvers: [
-      AppointmentsResolver,
-      CustomerResolver,
-    ],
-    emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
-    validate: {
-      forbidUnknownValues: false,
-    }
-  });
-
-  const server = new ApolloServer({
-    schema,
-  });
+  const server = await createServer();
 
   const { url } = await server.listen();
-
   console.log(`🚀 HTTP Server running on: ${url}`);
 }
 
